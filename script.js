300eb7b08531ec386306907d0f44df099d656944
asharifauzan <fauzansaja10@gmail.com>
2020-07-09 22:57:41 +0700

    membuat event ketika submit ditekan

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const input = document.getElementById('input');
 const submit = document.getElementById('submit');
 const close = document.getElementsByClassName('close');
+const ol = document.getElementsByTagName('ol')[0];
 
 submit.addEventListener('click', function() {
-  if (!input) {
-    alert("message");
+  if (!input.value) {
+    alert("Mohon isi list");
   }
+
+  let li = document.createElement('li');
+  let text = document.createTextNode(input.value);
+
+  li.appendChild(text);
+
+  ol.appendChild(li);
+
+  input.value = '';
 });
