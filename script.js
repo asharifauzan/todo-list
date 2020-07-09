d1e07e1a99d9f10b523fad3bef4eecda814c0d88
asharifauzan <fauzansaja10@gmail.com>
2020-07-09 23:54:15 +0700

    pindah position relative ke li

--- script.js
+++ script.js
@@ -11,9 +11,22 @@ submit.addEventListener('click', function() {
   let li = document.createElement('li');
   let text = document.createTextNode(input.value);
 
+  //menambah text pada li
   li.appendChild(text);
-
+  //menambah li pada ol
   ol.appendChild(li);
 
+  let span = document.createElement('span');
+  let x = document.createTextNode('');
+
+  //menambah class close pada span
+  span.setAttribute('class', 'close');
+
+  //menambah x ke span
+  span.appendChild(x);
+  //memasukkan li ke span
+  li.appendChild(span);
+
+  //reset input menjadi kosong
   input.value = '';
 });
